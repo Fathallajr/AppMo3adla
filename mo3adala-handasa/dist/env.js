e445d86de2713d2f2e903ec42869983e74490d35
Mahmoud Fathallah <212103670@o6u.edu.eg>
2026-02-12 23:32:08 +0200

    month 2

---
+++ mo3adala-handasa/dist/env.js
@@ -0,0 +1,22 @@
+// Runtime environment variables (override in deploys)
+window.NG_SITE_URL = window.NG_SITE_URL || 'https://example.com';
+window.NG_FORMSPREE_ENDPOINT = window.NG_FORMSPREE_ENDPOINT || 'https://formspree.io/f/XXXXXXX';
+window.NG_WHATSAPP = window.NG_WHATSAPP || '201554843745';
+window.NG_GISCUS_REPO = window.NG_GISCUS_REPO || '';
+window.NG_GISCUS_REPO_ID = window.NG_GISCUS_REPO_ID || '';
+window.NG_GISCUS_CATEGORY = window.NG_GISCUS_CATEGORY || '';
+window.NG_GISCUS_CATEGORY_ID = window.NG_GISCUS_CATEGORY_ID || '';
+// Socials - Updated with correct links
+window.NG_FB = window.NG_FB || 'https://www.facebook.com/Appmo3adla?mibextid=JpSO2FKliISFprGe';
+window.NG_FB_GROUP = window.NG_FB_GROUP || 'https://facebook.com/groups/575668737026816/';
+window.NG_YT = window.NG_YT || 'https://youtube.com/@app.mo3adla?si=lH73vGJ1r2X4P2TA';
+window.NG_IG = window.NG_IG || 'https://www.instagram.com/appmo3adla';
+window.NG_TT = window.NG_TT || 'https://www.tiktok.com/@appmo3adla?is_from_webapp=1&sender_device=pc';
+window.NG_TG = window.NG_TG || 'https://t.me/appmo3adla';
+// Platform
+window.NG_PLATFORM = window.NG_PLATFORM || 'https://app-mo3adlet-handsa.com/index.php';
+
+// WhatsApp direct chat link (overrides NG_WHATSAPP)
+window.NG_WA = window.NG_WA || 'https://api.whatsapp.com/send?phone=201554843745';
+
+
